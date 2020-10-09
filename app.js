8f578187336ea6fabf954f1ff4da92a8eb7b4bbb
srk2000 <71893333+srk2000@users.noreply.github.com>
2020-10-10 01:52:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+//to usen the contents in public directory
+
+
+app.get("/", (req,res)=> {
+	res.render("index.html");
+});
+
+// app.listen(3000,function(req,res){
+// 	console.log("hourlyattendance app started")
+// });
